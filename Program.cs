2095d62d665860eedbb28fcee8b1fff96ea9a4e0
unknown <revan.arturito@gmail.com>
2024-03-18 07:34:57 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -2,9 +2,39 @@
 {
     internal class Program
     {
+        class SimpleDataBase<T>
+        {
+            private List<T> storedData;
+            private List<DateTime> inputDates;
+
+            public SimpleDataBase()
+            {
+                this.storedData = new List<T>();    
+                this.inputDates = new List<DateTime>();
+            }
+
+            public void AddNewData(T data)
+            {
+                storedData.Add(data);
+                inputDates.Add(DateTime.Now);
+            }
+
+            public void PrintAllData()
+            {
+                for (int i = 0; i < storedData.Count;i++)
+                {
+                    Console.WriteLine($"Data {i+1} berisi: {storedData[i]}, yang disimpan pada waktu UTC {inputDates[i]}");
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SimpleDataBase<int> simple = new SimpleDataBase<int>();
+            simple.AddNewData(13);
+            simple.AddNewData(02);
+            simple.AddNewData(22);
+            simple.PrintAllData();
+            
         }
     }
 }
